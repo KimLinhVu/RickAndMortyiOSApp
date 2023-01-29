38f275a418012063b149c3eef25c158d3e53e395
KimLinhVu <klvbubble@gmail.com>
2023-01-29 14:36:28 -0800

    Add models

---
+++ RickAndMorty/Models/RMCharacterGender.swift
@@ -0,0 +1,15 @@
+//
+//  RMCharacterGender.swift
+//  RickAndMorty
+//
+//  Created by Kim-Linh Vu on 1/29/23.
+//
+
+import Foundation
+
+enum RMCharacterGender: String, Codable {
+    case female = "Female"
+    case male = "Male"
+    case genderless = "Genderless"
+    case `unknown` = ""
+}
---
+++ RickAndMorty/Models/RMOrigin.swift
@@ -0,0 +1,13 @@
+//
+//  RMOrigin.swift
+//  RickAndMorty
+//
+//  Created by Kim-Linh Vu on 1/29/23.
+//
+
+import Foundation
+
+struct RMOrigin: Codable {
+    let name: String
+    let url: String
+}
